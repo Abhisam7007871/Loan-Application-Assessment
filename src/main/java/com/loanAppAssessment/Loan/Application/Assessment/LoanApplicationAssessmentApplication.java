64ed36b3a13df543c789b7363aa137ef1b26d08f
Abhishek <abhisam7007871@gmail.com>
2024-03-24 19:49:43 +0530

    Init Assessment

---
+++ src/main/java/com/loanAppAssessment/Loan/Application/Assessment/LoanApplicationAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.loanAppAssessment.Loan.Application.Assessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanApplicationAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanApplicationAssessmentApplication.class, args);
+	}
+
+}
